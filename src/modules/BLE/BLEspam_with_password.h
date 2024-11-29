4310e4f5306cfad4a19b29a6a05a9b9da062ce6f
salvadordata <ak987tbram@icloud.com>
2024-11-29 09:55:03 -0500

    Update BLEspam_with_password.h

--- src/modules/BLE/BLEspam_with_password.h
+++ src/modules/BLE/BLEspam_with_password.h
@@ -1,11 +1,18 @@
 #ifndef BLESPAM_WITH_PASSWORD_H
 #define BLESPAM_WITH_PASSWORD_H
 
-#include "modules/BLE/BLESpam.h"
+#include <Arduino.h>
+#include "BLEDevice.h"
+#include "SdFat.h" // For SD card handling
 
-class BLESpamWithPassword : public BLESpam {
+class BLESpamWithPassword {
 public:
-    void aj_adv_with_password(int ble_choice, const String &password);
+    void aj_adv_with_password(int spamChoice, const String& password, const String& username = "");
+    void captureBLEInfo();
+    void saveToSD(const String& data);
+    void displayToScreen(const String& data);
+private:
+    void spamBLE(int choice);
 };
 
 #endif
