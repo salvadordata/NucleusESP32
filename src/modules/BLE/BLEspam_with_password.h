52d435fa1382064f458d6ff7c9e08a05bd608453
salvadordata <ak987tbram@icloud.com>
2024-11-30 05:50:21 -0500

    Update BLEspam_with_password.h

--- src/modules/BLE/BLEspam_with_password.h
+++ src/modules/BLE/BLEspam_with_password.h
@@ -2,17 +2,23 @@
 #define BLESPAM_WITH_PASSWORD_H
 
 #include <Arduino.h>
-#include "BLEDevice.h"
-#include "SdFat.h" // For SD card handling
 
 class BLESpamWithPassword {
 public:
     void aj_adv_with_password(int spamChoice, const String& password, const String& username = "");
+    void handleTouchInput(int x, int y); // Handle touch input for BLE spam selection
+    void drawSpamOptions(); // Display options for BLE spam modes
+    void setSpamChoice(int choice); // Set spam choice dynamically
+    void setPassword(const String& password); // Set password dynamically
+
+private:
+    void spamBLE(int choice);
     void captureBLEInfo();
     void saveToSD(const String& data);
     void displayToScreen(const String& data);
-private:
-    void spamBLE(int choice);
+
+    int currentSpamChoice = 1; // Default spam choice
+    String currentPassword = "DefaultPassword"; // Default password
 };
 
 #endif
